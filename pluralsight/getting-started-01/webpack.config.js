edd60729e9a63573fd42f61f7e249d76555dbc87
Aliaksandr Zabrodski <Aliaksandr.Zabrodski@iacapps.com>
2017-09-02 00:03:28 +0300

    Moved intro topics to  client-intro folder

--- pluralsight/getting-started-01/webpack.config.js
+++ pluralsight/getting-started-01/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './client/index.html',
+    template: './client-intro/index.html',
     filename: 'index.html',
     inject: 'body'
 });
