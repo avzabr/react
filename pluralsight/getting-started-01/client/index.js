f36a46ec481ebea51f138adad7bdf89a06226b77
Aliaksandr Zabrodski <Aliaksandr.Zabrodski@iacapps.com>
2017-09-01 23:43:28 +0300

    Added stage-0 preset. Added first react component with state

--- pluralsight/getting-started-01/client/index.js
+++ pluralsight/getting-started-01/client/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const Button = function (props) {
-    return (
-        <button>{props.label}</button>
-    )
-};
+class Button extends React.Component {
+
+    state = {counter: 0};
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>{this.state.counter}</button>
+        )
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1
+        }))
+
+    }
+
+}
 
 ReactDom.render(<Button label="Test"/>, document.getElementById('root'));
