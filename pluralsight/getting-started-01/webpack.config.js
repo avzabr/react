f94e264979783c6f84280efd108b9daf9e8dd4c4
Aliaksandr Zabrodski <Aliaksandr.Zabrodski@iacapps.com>
2017-09-04 13:22:45 +0300

    Added bootstrap 3

--- pluralsight/getting-started-01/webpack.config.js
+++ pluralsight/getting-started-01/webpack.config.js
@@ -1,31 +1,59 @@
 const path = require('path');
-const lerningModule = 'building-the-game-interface';
+const learningModule = 'building-the-game-interface';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './' + lerningModule + '/index.html',
+    template: './' + learningModule + '/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
+const ProvidePlugin = require('webpack/lib/ProvidePlugin');
+const ProvidePluginConfig = new ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ['popper.js', 'default']
+});
+
 module.exports = {
-    entry: './' + lerningModule + '/index.js',
+    entry: {
+        'main': './' + learningModule + '/index.js'
+    },
     output: {
         path: path.resolve('dist'),
         filename: 'index_bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
             // the url-loader uses DataUrls.
             // the file-loader emits files.
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff"
             },
-            {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
         ]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        ProvidePluginConfig,
+        HtmlWebpackPluginConfig,
+    ]
 };
